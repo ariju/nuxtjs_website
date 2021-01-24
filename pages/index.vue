64b323b6e7e98c1b59b96f2568046b8c5b2cb305
ariju <jun.0309ari@gmail.com>
2021-01-25 04:48:41 +0900

    画像の配置

--- pages/index.vue
+++ pages/index.vue
@@ -22,6 +22,9 @@
 
     <!-- Main Content -->
     <div class="container">
+    
+      <img src="~assets/images/vue.png" alt="">
+
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="post-preview">
@@ -56,6 +59,8 @@
         </div>
       </div>
     </div>
+
+
 
     
   </div>
